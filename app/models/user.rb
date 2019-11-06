3824d4d376848ba8ea2de5159416cd9596ba4fee
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-11-06 15:45:19 -0500

    completed self referential relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_many :paying_rent, foreign_key: :landlord_id, class_name: "Rent"
-  has_many :tenants, through: :paying_rent
+  has_many :receiving_rent, foreign_key: :landlord_id, class_name: "Rent"
+  has_many :tenants, through: :receiving_rent
+
+  has_many :paying_rent, foreign_key: :tenant_id, class_name: "Rent"
+  has_many :landlords, through: :paying_rent
 end
